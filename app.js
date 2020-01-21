e7539cb1a91194610116e0f676289fa607e49b14
rahulgurujala <isaacnewtonrahul@gmail.com>
2020-01-21 20:53:26 +0530

    fix

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-var users = requere("./dist/users.js");
-
+var users = [
+  { name: "Rahul", acNum: "111111", bal: "120000", pin: "1995" },
+  { name: "Govind", acNum: "222222", bal: "100000", pin: "1999" },
+  { name: "Priyanks", acNum: "333333", bal: "250000", pin: "2000" },
+  { name: "Supriya", acNum: "444444", bal: "50000", pin: "2002" }
+];
 const init = () => {
   let acNum, logBtn, acPin, selectOpt, logScreen;
   acNum = document.getElementById("acNum");
--- dist/users.js
+++
@@ -1,7 +0,0 @@
-var users = [
-  { name: "Rahul", acNum: "111111", bal: "120000", pin: "1995" },
-  { name: "Govind", acNum: "222222", bal: "100000", pin: "1999" },
-  { name: "Priyanks", acNum: "333333", bal: "250000", pin: "2000" },
-  { name: "Supriya", acNum: "444444", bal: "50000", pin: "2002" }
-];
-export default users;
